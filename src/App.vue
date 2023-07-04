4ab0edce41353d0a8b30dc0d08535096b6bf2f6f
Gianluca <122460487+gluke0@users.noreply.github.com>
2023-07-04 15:51:54 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script >
+
+</script>
+
+<template>
+  <h1>Ciao mondo</h1>
+</template>
+
+<style lang="scss">
+@use './style/main.scss';
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+// import './style.css'
+import App from './App.vue'
+
+//importare bootstrap js
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+
+createApp(App).mount('#app')
